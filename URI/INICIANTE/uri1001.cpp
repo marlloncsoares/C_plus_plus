c7a873e7ba5097f4871a60cb173b72f9e98adddd
Gallera Nerd <galleranerd@gmail.com>
2020-03-26 00:06:27 -0300

    Algoritmos

---
+++ URI/INICIANTE/uri1001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Algoritmo realiza a soma de dois números inteiros.
+
+int main() {
+    // Declara as variáveis que serão utilizadas.
+    int a, b, s;
+    // Realiza a leitura dos dados.
+    cin >> a >> b;
+    // Faz o processamento dos dados.
+    s = a + b;
+    // Mostra o resultado para o usuário.
+    cout << "X = " << s << endl;
+    return 0;
+}
---
+++ URI/INICIANTE/uri1003.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+
+// Algoritmo realiza a soma entre dois números inteiros.
+int main() {
+    // Declara as variáveis que serão utilizadas.
+    int a, b, soma;
+    // Realiza a leitura dos dados.
+    cin >> a >> b;
+    // Realiza o processamento dos dados.
+    soma = a + b;    
+    // Mostra o resultado final.
+    cout << "SOMA = " << soma << endl; 
+    return 0;
+
+}
